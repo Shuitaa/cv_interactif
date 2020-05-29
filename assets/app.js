be054f9e09d2a220d9f3021052871773561ba702
Pierre Averty <pierre.averty2001@gmail.com>
2020-05-29 15:22:29 +0200

    ajout du slider

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,15 @@
 "use strict";
 
 $(function(){
-    console.log("ya r ici")
+    $(".button-next").on("click",function(){
+        $(".button-previous").show();
+        $(".skill-content").css({"transform":"translateX(-240px)","transition":"transform 0.6s ease"});
+        $(".button-next").hide();
+    });
+    $(".button-previous").on("click",function(){
+        $(".button-next").show();
+        $(".skill-content").css({"transform":"translateX(0px)","transition":"transform 0.6s ease"})
+        $(".button-previous").hide();
+    });
 });
+
