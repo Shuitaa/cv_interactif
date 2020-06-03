3b069d77d1fcaeb0a76a8ba5a5ee9dc5f7d56df3
Pierre Averty <pierre.averty2001@gmail.com>
2020-06-03 13:02:52 +0200

    modifications

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,33 @@
 "use strict";
 
-$(function(){
+$(function () {
+    $(".skill-content").css({
+        "transform": "translateX(0px)"
+    });
     slider();
+    console.log("ça marche")
+    $(".circle-skill").each(function () {
+        var idSkill = $(this).attr("id");
+        $(this).toggleClass("circle-anim-" + idSkill);
+    });
+
 });
 
-function slider(){
-    $(".button-next").on("click",function(){
+function slider() {
+    $(".button-next").on("click", function () {
         $(".button-previous").show();
-        $(".skill-content").css({"transform":"translateX(-240px)","transition":"transform 0.6s ease"});
+        $(".skill-content").css({
+            "transform": "translateX(-456px)",
+            "transition": "transform 0.6s ease"
+        });
         $(".button-next").hide();
     });
-    $(".button-previous").on("click",function(){
+    $(".button-previous").on("click", function () {
         $(".button-next").show();
-        $(".skill-content").css({"transform":"translateX(0px)","transition":"transform 0.6s ease"})
+        $(".skill-content").css({
+            "transform": "translateX(0px)",
+            "transition": "transform 0.6s ease"
+        });
         $(".button-previous").hide();
     });
 }
